7b287d715eac006b167efd862f296c6d1d580a02
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-04-07 04:56:48 +0000

    Update load_anywhere_flask_app.py

--- load_anywhere_flask_app.py
+++ load_anywhere_flask_app.py
@@ -12,25 +12,7 @@ CORS(app)
 @app.route("/", methods=['GET', "OPTIONS"])
 @cross_origin()
 def index():
-    return """<html>
-<head>
-<meta charset="utf-8">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.11.2/brython.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.11.2/brython_stdlib.js"></script>
-</head>
-<body onload="brython()">
-<script type="text/python">
-from browser import document
-from browser.widgets.dialog import InfoDialog
-
-def click(ev):
-    InfoDialog("Hello", f"Hello, {document['zone'].value} !")
-
-# bind event 'click' on button to function echo
-document["echo"].bind("click", click)</script>
-<input id="zone">
-<button id="echo">click !</button>
-</body>
-</html>"""
+    return """
+<iframe frameborder="0" scrolling="no" src="https://raw.githubusercontent.com/ForkInABlender/ChatGPT_solutions_produced_from_simple_prompt/2023_04/test_index.html" width="100%"></iframe>"""
 
 app.run(host="0.0.0.0")
