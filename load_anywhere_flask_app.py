f2a3d5a07029691807990174a9153ecc47ecb654
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-04-07 05:30:39 +0000

    Update load_anywhere_flask_app.py

--- load_anywhere_flask_app.py
+++ load_anywhere_flask_app.py
@@ -1,17 +1,14 @@
-"""
-I don't know why this works even with blogspot.com, only that it works.
-
-"""
-
 from flask import Flask
 from flask_cors import CORS, cross_origin
+
+from requests import get
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=['GET', "OPTIONS"])
+@app.route("/", methods=['GET', "OPTIONS", "OPTIONS *"])
 @cross_origin()
 def index():
-    return """
-https://htmlpreview.github.io/?https://github.com/ForkInABlender/ChatGPT_solutions_produced_from_simple_prompt/blob/2023_04/test_index.html"""
+    return get("https://raw.githubusercontent.com/ForkInABlender/ChatGPT_solutions_produced_from_simple_prompt/2023_04/test_index.html").text
+
 app.run(host="0.0.0.0")
