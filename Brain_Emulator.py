f5aaa59d1e5f97dc8640700d6afed995e778acd7
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-05-31 20:04:16 +0000

    Update Brain_Emulator.py

--- Brain_Emulator.py
+++ Brain_Emulator.py
@@ -6,24 +6,13 @@ This code works and has been manually tested. Now what's left is the training se
 
 """
 
-
-
 from pybrain3.tools.shortcuts import buildNetwork
-from pybrain3.structure import FeedForwardNetwork
-from pybrain3.structure import LinearLayer, SigmoidLayer
 from pybrain3.structure import FullConnection
 
 # Emulating the Frontal Lobe
-frontal_lobe = FeedForwardNetwork()
-frontal_lobe.addInputModule(LinearLayer(10000, name="in"))
-frontal_lobe.addModule(SigmoidLayer(5000, name="hidden"))
-frontal_lobe.addOutputModule(LinearLayer(1000, name="out"))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['in'], frontal_lobe['hidden0']))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['hidden0'], frontal_lobe['out']))
-frontal_lobe.sortModules()
+frontal_lobe = buildNetwork(10000, 5000, 1000)
 
 # Emulating the Parietal Lobe
-
 parietal_lobe = buildNetwork(8000, 4000, 800)
 
 # Emulating the Temporal Lobe
@@ -33,49 +22,22 @@ temporal_lobe = buildNetwork(7500, 3500, 700)
 occipital_lobe = buildNetwork(6000, 3000, 600)
 
 # Emulating the Cerebellum
-cerebellum = FeedForwardNetwork()
-cerebellum.addInputModule(LinearLayer(9000, name="in"))
-cerebellum.addModule(SigmoidLayer(4500, name="hidden0"))
-cerebellum.addOutputModule(LinearLayer(900, name="out"))
-cerebellum.addConnection(FullConnection(cerebellum['in'], cerebellum['hidden0']))
-cerebellum.addConnection(FullConnection(cerebellum['hidden0'], cerebellum['out']))
-cerebellum.sortModules()
+cerebellum = buildNetwork(9000, 4500, 900)
 
 # Emulating the Brainstem
-brainstem = FeedForwardNetwork()
-brainstem.addInputModule(LinearLayer(3000, name="in"))
-brainstem.addModule(SigmoidLayer(1500, name="hidden0"))
-brainstem.addOutputModule(LinearLayer(300, name="out"))
-brainstem.addConnection(FullConnection(brainstem['in'], brainstem['hidden0']))
-brainstem.addConnection(FullConnection(brainstem['hidden0'], brainstem['out']))
-brainstem.sortModules()
+brainstem = buildNetwork(3000, 1500, 300)
 
 # Emulating the Limbic System
-limbic_system = FeedForwardNetwork()
-limbic_system.addInputModule(LinearLayer(5000, name="in"))
-limbic_system.addModule(SigmoidLayer(2500, name="hidden"))
-limbic_system.addOutputModule(LinearLayer(500, name="out"))
-limbic_system.addConnection(FullConnection(limbic_system['in'], limbic_system['hidden0']))
-limbic_system.addConnection(FullConnection(limbic_system['hidden0'], limbic_system['out']))
-limbic_system.sortModules()
+limbic_system = buildNetwork(5000, 2500, 500)
 
 # Emulating Wernicke's Area
-wernicke_area = FeedForwardNetwork()
-wernicke_area.addInputModule(LinearLayer(3500, name="in"))
-wernicke_area.addModule(SigmoidLayer(1800, name="hidden"))
-wernicke_area.addOutputModule(LinearLayer(350, name="out"))
-wernicke_area.addConnection(FullConnection(wernicke_area['in'], wernicke_area['hidden0']))
-wernicke_area.addConnection(FullConnection(wernicke_area['hidden0'], wernicke_area['out']))
-wernicke_area.sortModules()
+wernicke_area = buildNetwork(3500, 1800, 350)
 
 # Emulating Broca's Area
-broca_area = FeedForwardNetwork()
-broca_area.addInputModule(LinearLayer(3000, name="in"))
-broca_area.addModule(SigmoidLayer(1500, name="hidden"))
-broca_area.addOutputModule(LinearLayer(300, name="out"))
-broca_area.addConnection(FullConnection(broca_area['in'], broca_area['hidden0']))
-broca_area.addConnection(FullConnection(broca_area['hidden0'], broca_area['out']))
-broca_area.sortModules()
+broca_area = buildNetwork(3000, 1500, 300)
+
+# Emulating the Visual Cortex
+visual_cortex = buildNetwork(6000, 3000, 600)
 
 # Connecting the Frontal Lobe to other regions
 frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], parietal_lobe['in']))
@@ -135,15 +97,6 @@ broca_area.addConnection(FullConnection(broca_area['out'], occipital_lobe['in'])
 broca_area.addConnection(FullConnection(broca_area['out'], limbic_system['in']))
 broca_area.addConnection(FullConnection(broca_area['out'], wernicke_area['in']))
 
-# Emulating the Visual Cortex
-visual_cortex = FeedForwardNetwork()
-visual_cortex.addInputModule(LinearLayer(6000, name="in"))
-visual_cortex.addModule(SigmoidLayer(3000, name="hidden"))
-visual_cortex.addOutputModule(LinearLayer(600, name="out"))
-visual_cortex.addConnection(FullConnection(visual_cortex['in'], visual_cortex['hidden']))
-visual_cortex.addConnection(FullConnection(visual_cortex['hidden'], visual_cortex['out']))
-visual_cortex.sortModules()
-
 # Connecting the Visual Cortex to other regions
 visual_cortex.addConnection(FullConnection(visual_cortex['out'], frontal_lobe['in']))
 visual_cortex.addConnection(FullConnection(visual_cortex['out'], parietal_lobe['in']))
@@ -153,21 +106,9 @@ visual_cortex.addConnection(FullConnection(visual_cortex['out'], limbic_system['
 
 # Connecting the Frontal Lobe to the Visual Cortex
 frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], visual_cortex['in']))
-
-# Connecting the Parietal Lobe to the Visual Cortex
 parietal_lobe.addConnection(FullConnection(parietal_lobe['out'], visual_cortex['in']))
-
-# Connecting the Temporal Lobe to the Visual Cortex
 temporal_lobe.addConnection(FullConnection(temporal_lobe['out'], visual_cortex['in']))
-
-# Connecting the Occipital Lobe to the Visual Cortex
 occipital_lobe.addConnection(FullConnection(occipital_lobe['out'], visual_cortex['in']))
-
-# Connecting the Limbic System to the Visual Cortex
 limbic_system.addConnection(FullConnection(limbic_system['out'], visual_cortex['in']))
-
-# Connecting Wernicke's Area to the Visual Cortex
 wernicke_area.addConnection(FullConnection(wernicke_area['out'], visual_cortex['in']))
-
-# Connecting Broca's Area to the Visual Cortex
 broca_area.addConnection(FullConnection(broca_area['out'], visual_cortex['in']))
