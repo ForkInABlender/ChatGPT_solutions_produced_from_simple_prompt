e45aa5bdbc1e10c368c0e170aafb27eb85374e01
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-06-01 00:45:02 +0000

    Update snag_images_from_web.sh

--- snag_images_from_web.sh
+++ snag_images_from_web.sh
@@ -1,1 +1,5 @@
+# Dylan Kenneth Eliot+GPT+Dan-6.0-prompt
+
+# Using a triple quoted string, read from each url only the image URLs from <img> tags
+
 echo $1| while read -r url; do     curl -s "$url" | grep -o '<img[^>]*src="[^"]*"' | grep -o 'src="[^"]*"' | grep -o '"[^"]*"' | tr -d '"'; done
