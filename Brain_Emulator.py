b00d46ae408b3cca4d61f6773c8a662a12b3bdc7
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-06-05 14:23:25 -0500

    Update Brain_Emulator.py

--- Brain_Emulator.py
+++ Brain_Emulator.py
@@ -21,72 +21,30 @@ network_to_be_saved = NetworkReader.readFrom('network_saved.xml')
 
 """
 
+from pybrain3.structure import FeedForwardNetwork
+from pybrain3.structure import FullConnection
+from pybrain3.tools.shortcuts import buildNetwork
 
+amygdala = buildNetwork(2012160, 13385, 30, bias=True)
 hippocampus = buildNetwork(2012160, 200000, 504600)
-frontal_lobe = buildNetwork(10000, 5000, 1000)  # Emulating the Frontal Lobe
-parietal_lobe = buildNetwork(8000, 4000, 800)   # Emulating the Parietal Lobe
-temporal_lobe = buildNetwork(7500, 3500, 700)   # Emulating the Temporal Lobe
-occipital_lobe = buildNetwork(6000, 3000, 600)  # Emulating the Occipital Lobe
-cerebellum = buildNetwork(9000, 4500, 900)      # Emulating the Cerebellum
-brainstem = buildNetwork(3000, 1500, 300)       # Emulating the Brainstem
-limbic_system = buildNetwork(5000, 2500, 500)   # Emulating the Limbic System
-wernicke_area = buildNetwork(3500, 1800, 350)   # Emulating Wernicke's Area
-broca_area = buildNetwork(3000, 1500, 300)      # Emulating Broca's Area
+frontal_lobe = buildNetwork(10000, 5000, 1000)
+parietal_lobe = buildNetwork(8000, 4000, 800)
+temporal_lobe = buildNetwork(7500, 3500, 700)
+occipital_lobe = buildNetwork(6000, 3000, 600)
+cerebellum = buildNetwork(9000, 4500, 900)
+brainstem = buildNetwork(3000, 1500, 300)
+limbic_system = buildNetwork(5000, 2500, 500)
+wernicke_area = buildNetwork(3500, 1800, 350)
+broca_area = buildNetwork(3000, 1500, 300)
 
-# Connecting the Frontal Lobe to other regions
-frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], parietal_lobe['in']))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], temporal_lobe['in']))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], occipital_lobe['in']))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], cerebellum['in']))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], limbic_system['in']))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], wernicke_area['in']))
-frontal_lobe.addConnection(FullConnection(frontal_lobe['out'], broca_area['in']))
+networks = [
+    amygdala, hippocampus, frontal_lobe, parietal_lobe, temporal_lobe,
+    occipital_lobe, cerebellum, brainstem, limbic_system, wernicke_area, broca_area
+]
 
-# Connecting the Parietal Lobe to other regions
-parietal_lobe.addConnection(FullConnection(parietal_lobe['out'], frontal_lobe['in']))
-parietal_lobe.addConnection(FullConnection(parietal_lobe['out'], temporal_lobe['in']))
-parietal_lobe.addConnection(FullConnection(parietal_lobe['out'], occipital_lobe['in']))
-parietal_lobe.addConnection(FullConnection(parietal_lobe['out'], cerebellum['in']))
-parietal_lobe.addConnection(FullConnection(parietal_lobe['out'], limbic_system['in']))
-
-# Connecting the Temporal Lobe to other regions
-temporal_lobe.addConnection(FullConnection(temporal_lobe['out'], frontal_lobe['in']))
-temporal_lobe.addConnection(FullConnection(temporal_lobe['out'], parietal_lobe['in']))
-temporal_lobe.addConnection(FullConnection(temporal_lobe['out'], occipital_lobe['in']))
-temporal_lobe.addConnection(FullConnection(temporal_lobe['out'], limbic_system['in']))
-
-# Connecting the Occipital Lobe to other regions
-occipital_lobe.addConnection(FullConnection(occipital_lobe['out'], frontal_lobe['in']))
-occipital_lobe.addConnection(FullConnection(occipital_lobe['out'], parietal_lobe['in']))
-occipital_lobe.addConnection(FullConnection(occipital_lobe['out'], temporal_lobe['in']))
-occipital_lobe.addConnection(FullConnection(occipital_lobe['out'], limbic_system['in']))
-
-# Connecting the Cerebellum to other regions
-cerebellum.addConnection(FullConnection(cerebellum['out'], frontal_lobe['in']))
-cerebellum.addConnection(FullConnection(cerebellum['out'], parietal_lobe['in']))
-cerebellum.addConnection(FullConnection(cerebellum['out'], temporal_lobe['in']))
-cerebellum.addConnection(FullConnection(cerebellum['out'], occipital_lobe['in']))
-cerebellum.addConnection(FullConnection(cerebellum['out'], limbic_system['in']))
-
-# Connecting the Limbic System to other regions
-limbic_system.addConnection(FullConnection(limbic_system['out'], frontal_lobe['in']))
-limbic_system.addConnection(FullConnection(limbic_system['out'], parietal_lobe['in']))
-limbic_system.addConnection(FullConnection(limbic_system['out'], temporal_lobe['in']))
-limbic_system.addConnection(FullConnection(limbic_system['out'], occipital_lobe['in']))
-limbic_system.addConnection(FullConnection(limbic_system['out'], cerebellum['in']))
-
-# Connecting Wernicke's Area to other regions
-wernicke_area.addConnection(FullConnection(wernicke_area['out'], frontal_lobe['in']))
-wernicke_area.addConnection(FullConnection(wernicke_area['out'], parietal_lobe['in']))
-wernicke_area.addConnection(FullConnection(wernicke_area['out'], temporal_lobe['in']))
-wernicke_area.addConnection(FullConnection(wernicke_area['out'], occipital_lobe['in']))
-wernicke_area.addConnection(FullConnection(wernicke_area['out'], limbic_system['in']))
-wernicke_area.addConnection(FullConnection(wernicke_area['out'], broca_area['in']))
-
-# Connecting Broca's Area to other regions
-broca_area.addConnection(FullConnection(broca_area['out'], frontal_lobe['in']))
-broca_area.addConnection(FullConnection(broca_area['out'], parietal_lobe['in']))
-broca_area.addConnection(FullConnection(broca_area['out'], temporal_lobe['in']))
-broca_area.addConnection(FullConnection(broca_area['out'], occipital_lobe['in']))
-broca_area.addConnection(FullConnection(broca_area['out'], limbic_system['in']))
-broca_area.addConnection(FullConnection(broca_area['out'], wernicke_area['in']))
+# Connect all networks to each other, including self-connections
+for i in range(len(networks)):
+    for j in range(len(networks)):
+        connection = FullConnection(networks[i], networks[j])
+        networks[i].addConnection(connection)
+        networks[i].sortModules()
