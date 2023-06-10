9155ddf6ef7fb72f30ebf1f495c9469d2f4a87ab
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-06-09 21:09:47 -0500

    Update zip_mounting_from_url.py

--- zip_mounting_from_url.py
+++ zip_mounting_from_url.py
@@ -27,6 +27,13 @@ user_scripted_module_by_branch.function()
 
 ```
 
+This not only saves disk so you can do more than store modules locally, but also allows more flexibility.
+
+If you're having to import and check if you've installed the necessary packages everywhere you use your code,
+ well, this saves space by keeping it in as few places possible and prevents continuously installing. This also
+  makes it easier to track down bugs, and keep releases organized by version without over-writing someone else's
+ good code.
+
 """
 zip_path, _ = __import__(urllib.request.urlretrieve("http://example.com/path/to/yourfile.zip"))
 zipimporter = __import__("zipimport").zipimporter(zip_path)
