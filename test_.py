c18a70bfb6ff286ce5006084bb6f2f369e20ee46
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-09-12 18:43:08 -0500

    Add files via upload

---
+++ test_.py
@@ -0,0 +1,44 @@
+import os
+
+class ProcessMemory:
+    def __init__(self, pid):
+        self.pid = pid
+        self.mem_path = f"/proc/{pid}/mem"
+
+    def _validate_address(self, address):
+        if not isinstance(address, int) or address < 0:
+            raise ValueError("Invalid memory address provided.")
+
+    def read(self, address, length):
+        """Reads 'length' bytes from the specified 'address'."""
+        self._validate_address(address)
+        if length <= 0:
+            raise ValueError("Length must be a positive integer.")
+
+        try:
+            with open(self.mem_path, "rb") as mem_file:
+                mem_file.seek(address)
+                return mem_file.read(length)
+        except OSError as e:
+            print(f"Error reading memory at address {hex(address)}: {e}")
+            return None
+
+    def write(self, address, data):
+        """Writes 'data' to the specified 'address'."""
+        self._validate_address(address)
+        if not data:
+            raise ValueError("Data to write cannot be empty.")
+
+        try:
+            with open(self.mem_path, "wb") as mem_file:
+                mem_file.seek(address)
+                mem_file.write(data)
+        except OSError as e:
+            print(f"Error writing to memory at address {hex(address)}: {e}")
+
+
+pm = ProcessMemory(4165)
+data = pm.read(0x00007ff2f6dd3000, 5)
+if data:
+     print(data)
+pm.write(0x00007ff2f6dd3000, b"Hello")
