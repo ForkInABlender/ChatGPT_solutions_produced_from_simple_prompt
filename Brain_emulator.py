5fce4ce5955b8b049bb727ee12e9682189b648c9
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-10-27 19:32:53 -0500

    Update Brain_emulator.py

--- Brain_emulator.py
+++ Brain_emulator.py
@@ -9,14 +9,13 @@ The point of this was to display the 2-dimensional layout of a poly-dimensional 
 
 """
 
-
 from pybrain3.structure import TanhLayer, LSTMLayer, RecurrentNetwork, FullConnection
 from pybrain3.tools.shortcuts import buildNetwork
 from main__GPT_Model import net as gpt_net
 import numpy as np
 # Anterior Cingulate Cortex (ACC) Model
 acc_net = RecurrentNetwork()
-acc_net.addInputModule(TanhLayer(500, name='in'))
+acc_net.addInputModule(TanhLayer(100, name='in'))
 acc_net.addModule(TanhLayer(250, name='hidden0'))
 acc_net.addOutputModule(TanhLayer(50, name='out'))
 acc_net.addConnection(FullConnection(acc_net['in'], acc_net['hidden0']))
@@ -32,17 +31,17 @@ mirror_net.addConnection(FullConnection(mirror_net['hidden0'], mirror_net['out']
 mirror_net.addRecurrentConnection(FullConnection(mirror_net['hidden0'], mirror_net['hidden0']))
 # The rest for training
 insula_net = buildNetwork(500, 250, 50, hiddenclass=TanhLayer) 
-frontal_lobe_net = buildNetwork(1200, 600, 120)
-prefrontal_cortex = buildNetwork(1000, 500, 100, hiddenclass=TanhLayer)
-cerebral_cortex = buildNetwork(1000, 500, 100)
-temporal_lobe = buildNetwork(1000, 500, 250, hiddenclass=TanhLayer)
-visual_cortex = buildNetwork(800, 400, 80)
-brocas_area_net = buildNetwork(750, 375, 75)
-wernickes_area_net = buildNetwork(700, 350, 70)
+frontal_lobe_net = buildNetwork(100, 600, 120)
+prefrontal_cortex = buildNetwork(230, 500, 100, hiddenclass=TanhLayer)
+cerebral_cortex = buildNetwork(925, 500, 100)
+temporal_lobe = buildNetwork(50, 500, 250, hiddenclass=TanhLayer)
+visual_cortex = buildNetwork(120, 400, 80)
+brocas_area_net = buildNetwork(150, 375, 75)
+wernickes_area_net = buildNetwork(75, 350, 70)
 cerebellum = buildNetwork(700, 350, 70)
-brainstem = buildNetwork(600, 300, 60)
+brainstem = buildNetwork(120, 300, 60)
 hippocampus = buildNetwork(250, 125, 50, hiddenclass=LSTMLayer)
-thalamus = buildNetwork(400, 200, 40)
+thalamus = buildNetwork(400, 200, 120)
 amygdala = buildNetwork(120, 60, 10, hiddenclass=TanhLayer)
 hypothalamus = buildNetwork(200, 100, 20)
 # List of models
@@ -63,38 +62,17 @@ models = [
     ("thalamus", thalamus),
     ("amygdala", amygdala),
     ("hypothalamus", hypothalamus),
-		("gpt nlp", gpt_net)
+    ("gpt_net", gpt_net)
 ]
 for model_name, model in models:
     model.sortModules()
-    print("model: '{}'".format(model_name))
-    print(model)
-
-def one_hot_encode(word, vocab_size, char_to_int):
-    encoding = np.zeros((len(word), vocab_size))
-    for i, char in enumerate(word):
-        encoding[i, char_to_int[char]] = 1
-    return encoding
-
-def one_hot_decode(encoding, int_to_char):
-    decoded_word = ''
-    for row in encoding:
-        char_idx = np.argmax(row)
-        decoded_word += int_to_char[char_idx]
-    
-    return decoded_word
-
-vocab = 'abcdefghijklmnopqrstuvwxyz.?/\\\'\"\n\t\vABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(|) '
-char_to_int = {char: i for i, char in enumerate(vocab)}
-int_to_char = {i: char for char, i in char_to_int.items()}
-vocab_size = len(vocab)
-
 
 class BrainModel:
 	def __init__(self, models):
 		self.models = {name: model for name, model in models}
 		self.shared_memory = np.zeros(100)
 	def forward(self, sensory_input, previous_decision=None, social_input=None):
+		##
 		thalamus_output = self.models['thalamus'].activate(sensory_input)
 		emotional_state = self.models['amygdala'].activate(thalamus_output)
 		mirror_output = self.models['mirror_net'].activate(social_input if social_input is not None else np.zeros_like(sensory_input))
@@ -117,9 +95,10 @@ class BrainModel:
 
 brain = BrainModel(models)
 
-	# Example usage
+
 sensory_input = np.random.rand(400)  # Replace with actual sensory input
-previous_decision = np.random.rand(100)  # Replace with the previous decision if available
-social_input = np.random.rand(400)  # Replace with actual social input if available
+previous_decision = np.random.rand(250)  # Replace with the previous decision if available
+social_input = np.random.rand(500)  # Replace with actual social input if available
 
 final_decision, comprehension = brain.forward(sensory_input, previous_decision, social_input)
+print(final_decision, comprehension)
