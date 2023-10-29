ab20459f5d9423ce581c5f7b5bfc5055e781f132
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-10-28 21:30:10 -0500

    Update Brain_emulator.py

--- Brain_emulator.py
+++ Brain_emulator.py
@@ -3,8 +3,7 @@
 """
 
 The point of this was to display the 2-dimensional layout of a poly-dimensional structure like the brain. This can be used to develop an actual AI from
- human values. What it isn't for? Video games. :) In fact, it is intentionally not for video games. :)
-
+ human values. What it isn't for? Video games. :) In fact, it is intentionally not for video games. :) A human mind is not for video games. 
 
 """
 
