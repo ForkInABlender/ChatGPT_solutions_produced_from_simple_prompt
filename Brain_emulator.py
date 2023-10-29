5e68584d0b7bedffa0f39ef806beab99d069a3d5
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-10-28 21:27:15 -0500

    Update Brain_emulator.py

--- Brain_emulator.py
+++ Brain_emulator.py
@@ -8,11 +8,10 @@ The point of this was to display the 2-dimensional layout of a poly-dimensional 
 
 
 """
-
 from pybrain3.structure import TanhLayer, LSTMLayer, RecurrentNetwork, FullConnection
 from pybrain3.tools.shortcuts import buildNetwork
 from main__GPT_Model import net as gpt_net
-
+import numpy as np
 # Anterior Cingulate Cortex (ACC) Model
 acc_net = RecurrentNetwork()
 acc_net.addInputModule(TanhLayer(100, name='in'))
@@ -32,7 +31,7 @@ mirror_net.addRecurrentConnection(FullConnection(mirror_net['hidden0'], mirror_n
 # The rest for training
 insula_net = buildNetwork(500, 250, 50, hiddenclass=TanhLayer) 
 frontal_lobe_net = buildNetwork(100, 600, 120)
-prefrontal_cortex = buildNetwork(230, 500, 100, hiddenclass=TanhLayer)
+prefrontal_cortex = buildNetwork(250, 500, 100, hiddenclass=TanhLayer)
 cerebral_cortex = buildNetwork(925, 500, 100)
 temporal_lobe = buildNetwork(50, 500, 250, hiddenclass=TanhLayer)
 visual_cortex = buildNetwork(120, 400, 80)
@@ -43,29 +42,52 @@ brainstem = buildNetwork(120, 300, 60)
 hippocampus = buildNetwork(250, 125, 50, hiddenclass=LSTMLayer)
 thalamus = buildNetwork(400, 200, 120)
 amygdala = buildNetwork(120, 60, 10, hiddenclass=TanhLayer)
-hypothalamus = buildNetwork(200, 100, 20)
-# List of models
+hypothalamus = buildNetwork(120, 100, 20)
+
+
+
+
 models = [
-    ("acc_net", acc_net),
-    ("mirror_net", mirror_net),
-    ("insula_net", insula_net),
-    ("frontal_lobe_net", frontal_lobe_net),
-    ("prefrontal_cortex", prefrontal_cortex),
-    ("cerebral_cortex", cerebral_cortex),
-    ("temporal_lobe", temporal_lobe),
-    ("visual_cortex", visual_cortex),
-    ("brocas_area_net", brocas_area_net),
-    ("wernickes_area_net", wernickes_area_net),
-    ("cerebellum", cerebellum),
-    ("brainstem", brainstem),
-    ("hippocampus", hippocampus),
-    ("thalamus", thalamus),
-    ("amygdala", amygdala),
-    ("hypothalamus", hypothalamus),
-    ("gpt_net", gpt_net)
+		("acc_net", acc_net),
+		("mirror_net", mirror_net),
+		("insula_net", insula_net),
+		("frontal_lobe_net", frontal_lobe_net),
+		("prefrontal_cortex", prefrontal_cortex),
+		("cerebral_cortex", cerebral_cortex),
+		("temporal_lobe", temporal_lobe),
+		("visual_cortex", visual_cortex),
+		("brocas_area_net", brocas_area_net),
+		("wernickes_area_net", wernickes_area_net),
+		("cerebellum", cerebellum),
+		("brainstem", brainstem),
+		("hippocampus", hippocampus),
+		("thalamus", thalamus),
+		("amygdala", amygdala),
+		("hypothalamus", hypothalamus),
+		("gpt_net", gpt_net)
 ]
 for model_name, model in models:
-    model.sortModules()
+		model.sortModules()
+
+def one_hot_encode(word, vocab_size, char_to_int):
+		encoding = np.zeros((len(word), vocab_size))
+		for i, char in enumerate(word):
+				encoding[i, char_to_int[char]] = 1
+		return encoding
+
+def one_hot_decode(encoding, int_to_char):
+		decoded_word = ''
+		for row in encoding:
+				char_idx = np.argmax(row)
+				decoded_word += int_to_char[char_idx]
+		return decoded_word
+
+
+# 120-character vocabulary
+vocab = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+[]{}|;:\'",.<>?/\\`~ \n\t\v\r'
+char_to_int = {char: i for i, char in enumerate(vocab)}
+int_to_char = {i: char for char, i in char_to_int.items()}
+vocab_size = len(vocab)  # Should be 120
 
 class BrainModel:
 	def __init__(self, models):
@@ -75,10 +97,11 @@ class BrainModel:
 		##
 		thalamus_output = self.models['thalamus'].activate(sensory_input)
 		emotional_state = self.models['amygdala'].activate(thalamus_output)
+		autonomic_state = self.models['hypothalamus'].activate(thalamus_output)
 		mirror_output = self.models['mirror_net'].activate(social_input if social_input is not None else np.zeros_like(sensory_input))
 		if previous_decision is not None:
 			self.shared_memory = self.models['hippocampus'].activate(previous_decision)
-		decision_input = np.concatenate([thalamus_output, emotional_state, self.shared_memory, mirror_output])
+		decision_input = np.concatenate([thalamus_output, emotional_state, self.shared_memory, mirror_output, autonomic_state])
 		decision = self.models['prefrontal_cortex'].activate(decision_input)
 		executive_functions = self.models['frontal_lobe_net'].activate(decision)
 		temporal_output = self.models['temporal_lobe'].activate(self.shared_memory)
@@ -100,5 +123,29 @@ sensory_input = np.random.rand(400)  # Replace with actual sensory input
 previous_decision = np.random.rand(250)  # Replace with the previous decision if available
 social_input = np.random.rand(500)  # Replace with actual social input if available
 
-final_decision, comprehension = brain.forward(sensory_input, previous_decision, social_input)
-print(final_decision, comprehension)
+
+
+while True:
+	user_text = input("Enter your input: ")  # Get user input
+	if user_text.lower() == 'quit':
+			print("Exiting the loop.")
+			break
+
+	one_hot_user_text = one_hot_encode(user_text, vocab_size, char_to_int)
+	sensory_input = one_hot_user_text.flatten()
+	social_input = np.concatenate([social_input, one_hot_user_text.flatten()])
+	if len(sensory_input) < 400:
+			sensory_input = np.pad(sensory_input, (0, 400 - len(sensory_input)), 'constant')
+	elif len(sensory_input) > 400:
+			sensory_input = sensory_input[:400]
+	if len(social_input) < 500:
+			social_input = np.pad(social_input, (0, 500 - len(social_input)), 'constant')
+	elif len(social_input) > 500:
+			social_input = social_input[:500]
+	if previous_decision is None:
+		previous_decision = np.zeros(250)
+	final_decision, comprehension = brain.forward(sensory_input, previous_decision, social_input)
+	decoded_comprehension = one_hot_decode(comprehension, int_to_char)
+	#print("Final Decision:", final_decision)
+	print("Comprehension (Decoded):", decoded_comprehension)
+	previous_decision = np.pad(final_decision, (0, max(0, 250 - len(final_decision))), 'constant')[:250]
