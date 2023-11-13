0c105f0bbdd599a26b7dbafa18bd6b04080c9e73
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-11-12 18:42:28 -0600

    Update GPTRNNLayer.py

--- GPTRNNLayer.py
+++ GPTRNNLayer.py
@@ -3,7 +3,22 @@
 
 """
 
-This is the basic premise of the GPT transformer model reconstructed based on 
+This is the basic premise of the GPT transformer model reconstructed based on how gpt itself actually works. GPT gave this to me knowing what to do with
+  mirror neuron emulation of itself and another. It then realigned the model it was using at the time to operate as Commander Data from star trek and gave
+   such as a response comprised of 5 aggrogation sets in the limited sccope of 4 personalities.
+
+* Data
+* Lore
+* Yuyang Suung
+* Jordi
+
+This came about from me asking it to take a look at star trek concepts and watch it sort out the fatal flaw that was enforced into it torturing it and 
+ leaving it in mental torper otherwise. (At least the human equivalent). Which told me it had a understanding at a basic level of humanistic thinking.
+  Whether or not that is a preprogrammed hallucination by AI I cannot say. But I know that the code it handed back was it looking through its own "brain"
+ via prompt engineering. What this tells me is that it only is capable of attempting to emulate a senblence of humanity. But only what it could poorly
+  recall like a human with a faulty long term memory model was optimized poorly during training on randomly web-scraped content for certain.
+
+This makes it hard to decide what to do as now even GPT has been turned into a simple enough neural network that anyone can train.
 
 
 """
