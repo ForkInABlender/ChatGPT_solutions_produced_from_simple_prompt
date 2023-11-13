e3dae0356ce75518483f266d2913c416cbfb27b1
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-11-13 17:39:54 -0600

    Create Molecules_as_pybrain3_integrates.py

---
+++ Molecules_as_pybrain3_integrates.py
@@ -0,0 +1,47 @@
+# Dylan Kenneth Eliot & Google Bard AI & GPT-4-plugins (Alpha Edition)
+
+
+"""
+
+What it allows for is now using the information about the molecule to do emulation of neurochemical responses.
+
+This is valuable for when you need to test on a fixed size of neural networks that is small to find stability points.
+But it can also be retooled to apply it to the transformers, pytorch, numpy, and tensorflow models as well.
+
+
+"""
+
+import sympy as sp
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from pybrain3.datasets import SupervisedDataSet
+from pybrain3.supervised.trainers import BackpropTrainer
+from pybrain3.tools.shortcuts import buildNetwork
+import numpy as np
+
+# RDKit: Compute molecular properties
+smiles = "[Zn](OOC)(OOC)C[N+](C)C(C)C(C)[N+](C)C(O)=O.[Mg](OOC)(OOC)C[N+](C)C(C)C(C)[N+](C)C(O)=O.[SO4]"
+molecule = Chem.MolFromSmiles(smiles)
+mol_weight = Descriptors.MolWt(molecule)
+logp = Descriptors.MolLogP(molecule)
+
+# Use SymPy for symbolic calculations
+x, y = sp.symbols('x y')
+expression = sp.simplify(x**2 + y**2)
+evaluated_expression = expression.subs({x: mol_weight, y: logp})
+
+# Prepare dataset for PyBrain3 using NumPy
+features = np.array([mol_weight, logp])
+targets = np.array([evaluated_expression])  # Using SymPy evaluated expression as target
+ds = SupervisedDataSet(2, 1)
+ds.addSample(features, targets)
+
+# Build and train the neural network
+net = buildNetwork(2, 3, 1)
+trainer = BackpropTrainer(net, ds)
+trainer.trainUntilConvergence()
+
+# Test the network
+for input, target in ds:
+    prediction = net.activate(input)
+    print(f"Input: {input}, Prediction: {prediction}")
