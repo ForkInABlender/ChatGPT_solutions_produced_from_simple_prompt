1054ac8d352c1bcb1aeeb8f5635ec7261ac0cf8e
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-11-16 19:19:35 -0600

    Update Molecules_as_pybrain3_integrates.py

--- Molecules_as_pybrain3_integrates.py
+++ Molecules_as_pybrain3_integrates.py
@@ -1,17 +1,15 @@
-# Dylan Kenneth Eliot & Google Bard AI & GPT-4-plugins (Alpha Edition)
+  GNU nano 7.2                                            app.py                                                     # Dylan Kenneth Eliot & Google Bard AI & GPT-4-plugins (Alpha Edition)
 
 
 """
 
 What it allows for is now using the information about the molecule to do emulation of neurochemical responses.
 
-This is valuable for when you need to test on a fixed size of neural networks that is small to find stability points.
-But it can also be retooled to apply it to the transformers, pytorch, numpy, and tensorflow models as well.
+This is valuable for when you need to test on a fixed size of neural networks that is small to find stability points.But it can also be retooled to apply it to the transformers, pytorch, numpy, and tensorflow models as well.
 
 Now the smile below is of Crispr or the common substructure used in DNA repair mediated by zinc and magnesium ions.
 
-What I'd like for to be done with this is for it to be used to find stable DNA repairs and how they apply to the brain via emulation.
-
+What I'd like for to be done with this is for it to be used to find stable DNA repairs and how they apply to the bra>
 
 """
 
@@ -22,28 +20,29 @@ from pybrain3.datasets import SupervisedDataSet
 from pybrain3.supervised.trainers import BackpropTrainer
 from pybrain3.tools.shortcuts import buildNetwork
 import numpy as np
-
 # RDKit: Compute molecular properties
-smiles = "[Zn](OOC)(OOC)C[N+](C)C(C)C(C)[N+](C)C(O)=O.[Mg](OOC)(OOC)C[N+](C)C(C)C(C)[N+](C)C(O)=O.[SO4]"
+smiles = "CC(=O)O"
 molecule = Chem.MolFromSmiles(smiles)
 mol_weight = Descriptors.MolWt(molecule)
 logp = Descriptors.MolLogP(molecule)
-
-# Use SymPy for symbolic calculations
 x, y = sp.symbols('x y')
 expression = sp.simplify(x**2 + y**2)
 evaluated_expression = expression.subs({x: mol_weight, y: logp})
-
-# Prepare dataset for PyBrain3 using NumPy
 features = np.array([mol_weight, logp])
-targets = np.array([evaluated_expression])  # Using SymPy evaluated expression as target
+targets = np.array([evaluated_expression])
+
+print(features)
+print(targets)
+
+
 ds = SupervisedDataSet(2, 1)
 ds.addSample(features, targets)
 
-# Build and train the neural network
 net = buildNetwork(2, 3, 1)
 trainer = BackpropTrainer(net, ds)
-trainer.trainUntilConvergence()
+
+for a in range(1000):
+    trainer.train()
 
 # Test the network
 for input, target in ds:
