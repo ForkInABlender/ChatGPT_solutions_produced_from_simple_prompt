4ad9d68a86add8d9d694ca266c25d2ec5c69be03
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-11-18 21:12:02 -0600

    Update golang_compiled_library_in_python.py

--- golang_compiled_library_in_python.py
+++ golang_compiled_library_in_python.py
@@ -33,28 +33,15 @@ func HelloWorld() {
 func main() {}
 """
 
-# Create a temporary directory
 temp_dir = tempfile.mkdtemp()
-
-# Save the Go code to a temporary file
 with tempfile.NamedTemporaryFile(dir=temp_dir, mode="w", delete=False, suffix=".go") as go_file:
     go_file.write(go_code)
     go_file_path = go_file.name
-
-# Compile the Go code into a shared library
 so_file_path = go_file_path.replace(".go", ".so")
 subprocess.run(["go", "build", "-o", so_file_path, "-buildmode=c-shared", go_file_path])
-
-# Load the shared library dynamically
+os.remove(go_file_path)
 hello = ctypes.CDLL(so_file_path)
-
-# Define types explicitly for clarity
+os.remove(so_file_path)
 hello.HelloWorld.argtypes = []
 hello.HelloWorld.restype = ctypes.c_void_p
-
-# Call the Go function
 hello.HelloWorld()
-
-# Clean up: remove temporary files
-os.remove(go_file_path)
-os.remove(so_file_path)
