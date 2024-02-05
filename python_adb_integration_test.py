b0a1f6a13b16def622838de8100eea8b04cac787
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-02-05 00:05:33 -0600

    Update python_adb_integration_test.py

--- python_adb_integration_test.py
+++ python_adb_integration_test.py
@@ -9,6 +9,12 @@ pip install pure-python-adb adb-shell
 What this does is allow one to interact with a android device using ADB. This allows one to use python to interact with software running on an android device by its serial id.
 
 This also lets you make use of anything adb normally touches on.
+
+The overrides applied to the shell and more.
+
+I won't go into more details on how as that override can apply even to golang functions integrated also into python. 
+
+https://g.co/bard/share/260ae988f242
 """
 
 from ppadb.client import Client as AdbClient
