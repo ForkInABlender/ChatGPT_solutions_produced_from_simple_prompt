772c1e454d9b9bf04bfb05c94663bdd5e85a52fe
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-02-10 02:31:26 -0600

    Update mc_py_script.sh

--- mc_py_script.sh
+++ mc_py_script.sh
@@ -1,3 +1,13 @@
 # Dylan Kenneth Eliot
 
+"""
+
+use this with the prior script within minecraft-launcher. replace the java runtime binary literally with the path to this script.
+
+This will let you run from python3.10 your minecraft game with whatever python modding you apply to your games' runtime.
+
+"""
+
+
+
 python3.10 /path/to/mc_py_script.py $@
