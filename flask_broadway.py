bc8b88fddf99422abf549c8f2d6ba074c823b5b9
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-02-10 17:00:17 -0600

    Add files via upload

---
+++ flask_broadway.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    url = 'http://0.0.0.0:5001'  # URL of the broadwayd service
+    response = requests.get(url)
+    content = response.text  # or response.json() if JSON data
+    # Process and use the content as needed, e.g., pass to a template
+    return render_template('your_template.html', content=content)
+
+if __name__ == '__main__':
+    app.run(port=5002, debug=True)
