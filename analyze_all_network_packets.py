498fd45f2b5313b659cf2b42320fda7fc44e00f9
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-02-15 22:56:22 -0600

    Update analyze_all_network_packets.py

--- analyze_all_network_packets.py
+++ analyze_all_network_packets.py
@@ -8,7 +8,7 @@ import textwrap
 
 This is a little more advanced. It looks at all packets then decodes what its eyeball sees.
 
-
+This script works best with root or proot (root like environment even on / ) like shell environments.
 """
 
 
