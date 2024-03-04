c405774da2e557805dead54b6c88dd8abfac744c
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-03-03 23:57:17 -0600

    Update updated_main.py

--- pybrain3_custom_layers/updated_main.py
+++ pybrain3_custom_layers/updated_main.py
@@ -12,7 +12,7 @@ The last edits to make require edits to the ``MolecularNeuroModule`` class so on
 
 import os
 
-tiktoken_cache_dir = "/app/"
+tiktoken_cache_dir = "./"
 os.environ["TIKTOKEN_CACHE_DIR"] = tiktoken_cache_dir
 assert os.path.exists(os.path.join(tiktoken_cache_dir,"9b5ad71b2ce5302211f9c61530b329a4922fc6a4"))
 
