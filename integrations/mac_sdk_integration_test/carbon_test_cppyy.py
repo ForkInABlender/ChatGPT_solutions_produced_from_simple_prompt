68c1b53ffcf487d781e9672c866b6e5e2d7c6d73
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-03-08 22:54:54 -0600

    Create carbon_test_cppyy.py

---
+++ integrations/mac_sdk_integration_test/carbon_test_cppyy.py
@@ -0,0 +1,17 @@
+# Dylan Kenneth Eliot & GPT-4-Plugins ( Alpha Edition )
+
+"""
+
+
+"""
+
+import cppyy
+cppyy.add_include_path("./")
+cppyy.cppdef("""
+#define TARGET_CPU_X86_64 1
+#include "./MacTypes.h"
+UInt32 add(UInt32 a, UInt32 b) {
+    return a + b;
+}
+""")
+print(f"Result: {cppyy.gbl.add(10, 20)}")
