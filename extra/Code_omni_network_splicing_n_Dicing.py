261c6c061e057ca5ccfe7364c7885b9fa78e18e1
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-04-30 20:47:15 -0500

    Create Code_omni_network_splicing_n_Dicing.py

---
+++ extra/Code_omni_network_splicing_n_Dicing.py
@@ -0,0 +1,57 @@
+#Dylan Kenneth Eliot & GPT-4 ( Alpha-Edition )
+
+"""
+
+
+This is code that can be used to template network improvements from.
+
+This type of approach would be good for reducing cost of infrastructure.
+
+In addition, it is useful to know for a dynamic network with real time network monitoring.
+
+"""
+
+import networkx as nx
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+
+class SimpleSDNApp(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSDNApp, self).__init__(*args, **kwargs)
+        self.topology = nx.DiGraph()
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, MAIN_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Install the table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+# Example of creating a network graph with NetworkX
+G = nx.DiGraph()
+G.add_edge('switch1', 'switch2', capacity=10)
+G.add_edge('switch2', 'switch3', capacity=15)
+paths = nx.all_simple_paths(G, source='switch1', target='switch3')
+print(list(paths))
