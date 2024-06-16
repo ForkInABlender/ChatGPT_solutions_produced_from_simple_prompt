1c63621c7838d5a3cf3999319324ebd7604d948f
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-06-16 01:43:30 -0500

    Update dockerd_mimick.py

--- integrations/python based dockerd/dockerd_mimick.py
+++ integrations/python based dockerd/dockerd_mimick.py
@@ -11,7 +11,10 @@ Product class:
      potential: limit unknown
 
 The purpose was to explore an option that was more conformant the the logic of docker and not so much as be stuck to architectural limitations.
- But instead have a clear path that allowed for running even on a cellphone running python inside android.  
+ But instead have a clear path that allowed for running even on a cellphone running python inside android. With that being said, dockerd's remaining logic
+  could be implemented in python with golang & ctypes interfacing now that I've figured out the hard part.
+
+The next steps is it needs unicorn-engine and kernel emulation. Plus more documentation surfing. 
 
 """
 
