10c17ad716f78052fb0788b01f503ae752a0ffbb
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-07-07 06:00:26 -0500

    Update data_encoder.py

--- pybrain3_custom_layers/MAML/fragment-[0x3]/data_encoder.py
+++ pybrain3_custom_layers/MAML/fragment-[0x3]/data_encoder.py
@@ -1,41 +1,27 @@
 # Dylan Kenneth Eliot & GPT-4o ( Alpha Edition )
 
 """
-Way to encode and decode data
+This is a way to encode and decode data from an image.
 
 
 """
 
-def char_to_rgba(char):
-    # Unicode value of the character
-    unicode_value = ord(char)
-    R = (unicode_value & 0xFF000000-0xc0) >> 6
-    G = (unicode_value & 0x00FF0000-0x30) >> 4
-    B = (unicode_value & 0x0000FF00-0x0c) >> 2
-    A = (unicode_value & 0x000000FF-0x30)
-    return [R, G, B, A]
-
-def string_to_rgba_array(input_string):
-    rgba_array = [char_to_rgba(char) for char in input_string]
-    return rgba_array
-
-# Example usage
-input_string = "事有不你?"
-rgba_array = string_to_rgba_array(input_string)
-
-print(rgba_array)
-
-def rgba_to_char(rgba):
-    ascii_value = (rgba[0] << 6) | (rgba[1] << 4) | (rgba[2] << 2) | rgba[3]
+def ordinal_to_rgba(ordinal):
+    ordinal = ordinal & 0xFFFFFFFF
     
-    # Convert ASCII value back to character
-    return chr(ascii_value)
+    r = int(ordinal & 0xFF)
+    g = int((ordinal >> 8) & 0xFF)
+    b = int((ordinal >> 16) & 0xFF)
+    a = int((ordinal >> 24) & 0xFF)
 
-def rgba_array_to_string(rgba_array):
-    decoded_string = ''.join(rgba_to_char(rgba) for rgba in rgba_array)
-    return decoded_string
+    return r, g, b, a
 
-decoded_string = rgba_array_to_string(rgba_array)
-
-# Print the result
-print(decoded_string)
+def rgba_to_ordinal(r, g, b, a):
+    r = int(r) & 0xFF
+    g = int(g) & 0xFF
+    b = int(b) & 0xFF
+    a = int(a) & 0xFF
+    
+    ordinal = (a << 24) | (b << 16) | (g << 8) | r
+    
+    return ordinal
