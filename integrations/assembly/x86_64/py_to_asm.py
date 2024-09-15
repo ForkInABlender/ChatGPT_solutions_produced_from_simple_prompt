46d935100d064592364cbabf241e18c2d907fd60
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-09-15 14:14:06 -0500

    Create py_to_asm.py

---
+++ integrations/assembly/x86_64/py_to_asm.py
@@ -0,0 +1,28 @@
+# Dylan Kenneth Eliot & GPT-4o ( Alpha Edition )
+
+"""
+This allows for "compiling down" each function into basic assembly, plus some llvm+numba+gnu-gcc boilerplate glue-code.
+
+"""
+
+import numba
+from numba import jit, int32
+from llvmlite import binding
+
+# Initialize llvmlite binding
+binding.initialize()
+binding.initialize_native_target()
+binding.initialize_native_asmprinter()
+
+@jit(int32(int32, int32), nopython=True)
+def example_function(x, y):
+    return x * y + 10  # Simple arithmetic operation
+
+llvm_ir = example_function.inspect_llvm(example_function.signatures[0])
+
+target = binding.Target.from_default_triple()
+target_machine = target.create_target_machine(opt=3, features="")
+llvm_module = binding.parse_assembly(llvm_ir)
+assembly_code = target_machine.emit_assembly(llvm_module)
+
+print(assembly_code)
