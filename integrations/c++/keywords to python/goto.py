9712b48bd37112b084f6a46e2d464812dd5e009e
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-09-17 05:33:50 -0500

    Update goto.py

--- integrations/c++/keywords to python/goto.py
+++ integrations/c++/keywords to python/goto.py
@@ -6,40 +6,40 @@ In this version of goto.py, it uses exceptions and labels have no arguments.
 
 
 """
-
 class Goto(Exception):
-	pass
+    pass
 
-def goto(label, globals_):
-	if label in globals_:
-			func = globals_[label]
-			func()
-	else:
-			raise ValueError(f"Label {label} not defined")
+def goto(label, globals_=globals()):
+    if label in globals_:
+        func = globals_[label]
+        func()
+    else:
+        raise ValueError("Label %s not defined" % label)
 
 def label(func):
-	def wrapper(*args, **kwargs):
-			try:
-					return func(*args, **kwargs)
-			except Goto as jump:
-					if jump.args[0] == func.__name__:
-							return wrapper(*args, **kwargs)
-					else:
-							raise
-	return wrapper
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Goto as jump:
+            if jump.args[0] == func.__name__:
+                return wrapper(*args, **kwargs)
+            else:
+                raise
+    return wrapper
 
 # Usage example
 @label
 def start():
-	print("This is the start.")
-	goto("end", globals())
+    print("This is the start.")
+    goto("end")
+    goto("middle")
 
 @label
 def middle():
-	print("This is the middle.")
+    print("This is the middle.")
 
 @label
 def end():
-	print("This is the end.")
+    print("This is the end.")
 
 start()
