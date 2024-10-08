8ce9a59e7b4a6127c76669ca5fa2cc0228d5bc66
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2024-10-08 07:11:24 -0500

    Delete integrations/c++/keywords to python/namespace/types.py

--- integrations/c++/keywords to python/namespace/types.py
+++
@@ -1,8 +0,0 @@
-import types
-import typing
-
-class Namespace(types.SimpleNamespace):
-    __setitem__ = types.SimpleNamespace.__setattr__
-
-    def __call__(self, entity: typing.Union[types.FunctionType, typing.Type]) -> None:
-        self[entity.__name__] = entity
