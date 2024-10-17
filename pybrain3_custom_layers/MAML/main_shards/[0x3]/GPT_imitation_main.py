20f45a3b9de600e4abfcdb0e033798528edbcdeb
Dylan Kenneth Eliot <92487561+ForkInABlender@users.noreply.github.com>
2024-10-17 10:59:50 -0500

    Create GPT_imitation_main.py

---
+++ pybrain3_custom_layers/MAML/main_shards/[0x3]/GPT_imitation_main.py
@@ -0,0 +1,50 @@
+# Dylan Kenneth Eliot
+"""
+This creates with the Dim3NeuronLayer as the hidden layers. This should form the basic template for GPT knowing that the Dim3NeuronLayer does the QKV as well as 3d, 2d, & 1d processing of data.
+
+"""
+
+from pybrain3.structure import FeedForwardNetwork, LinearLayer, BiasUnit
+from pybrain3.structure.connections import FullConnection
+##############################################
+from dim3_neuronlayer import Dim3NeuronLayer #
+##############################################
+from pybrain3.datasets import SupervisedDataSet
+
+net = FeedForwardNetwork()
+
+input_size = 50257
+output_size = 50257
+num_hidden_layers = 30
+
+indim, outdim, num_heads = 768, 768, 256
+
+in_layer = LinearLayer(input_size)
+out_layer = LinearLayer(output_size)
+bias = BiasUnit()
+
+net.addInputModule(in_layer)
+net.addOutputModule(out_layer)
+net.addModule(bias)
+
+previous_layer = in_layer
+
+for i in range(num_hidden_layers):
+    hidden_layer = Dim3NeuronLayer(indim=indim, outdim=outdim, num_heads=num_heads, name=f"HiddenLayer_{i+1}")
+    net.addModule(hidden_layer)
+
+    net.addConnection(FullConnection(previous_layer, hidden_layer))
+    net.addConnection(FullConnection(bias, hidden_layer))
+
+    previous_layer = hidden_layer
+
+net.addConnection(FullConnection(previous_layer, out_layer))
+net.addConnection(FullConnection(bias, out_layer))
+
+net.sortModules()
+
+input_data = [0.0] * input_size
+
+output = net.activate(input_data)
+
+print("Network Output:", output)
