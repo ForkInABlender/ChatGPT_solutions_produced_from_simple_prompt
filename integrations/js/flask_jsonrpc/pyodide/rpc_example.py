7409c5e9688b0a2a36c7b3b43304f72f017efff9
Dylan Kenneth Eliot <92487561+ForkInABlender@users.noreply.github.com>
2024-11-27 03:57:31 -0600

    Update rpc_example.py

--- integrations/js/flask_jsonrpc/pyodide/rpc_example.py
+++ integrations/js/flask_jsonrpc/pyodide/rpc_example.py
@@ -91,5 +91,5 @@ loadPyodideAndPackages();
 def add(a: int, b: int) -> int:
 		return a + b
 
-if __name__ == '__main__':
-		app.run(host='0.0.0.0', port=5000, debug=True)
+#if __name__ == '__main__':
+#		app.run(host='0.0.0.0', port=5000, debug=True)
