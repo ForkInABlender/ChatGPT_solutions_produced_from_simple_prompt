901c581cd2776746fba5d92ccc22e0d7b9cd282f
Dylan Kenneth Eliot <92487561+ForkInABlender@users.noreply.github.com>
2025-09-13 10:22:19 -0500

    Update AI_neurons.py

--- pybrain3_custom_layers/AI_neurons.py
+++ pybrain3_custom_layers/AI_neurons.py
@@ -20,20 +20,16 @@ from rdkit.Chem import Descriptors
 from pybrain3.datasets import SupervisedDataSet
 from pybrain3.supervised.trainers import BackpropTrainer
 from pybrain3.tools.shortcuts import buildNetwork
-from brian2 import *
+import random
 
 # RDKit: Compute molecular properties
 smiles = "CC(=O)O"
 molecule = Chem.MolFromSmiles(smiles)
-molwt = Descriptors.MolWt(molecule)
-logp = Descriptors.MolLogP(molecule)
+molwt = Descriptors.MolWt(molecule) #molwt=x['amw']             # molecular weight term
+logp = Descriptors.MolLogP(molecule) #logp=x['CrippenClogP']     # partition coefficient
 
-print(smiles, molecule, molwt, log)
+print(smiles, molecule, molwt, logp)
 
-#####
-
-start_scope()
-duration = 100*ms
 eqs = '''
 dv/dt = (molwt + logp - v)/tau : 1
 tau : second
@@ -42,26 +38,82 @@ print(eqs)
 input_neurons = 2
 hidden_neurons_1 = 3
 output_neurons = 1
-G = NeuronGroup(input_neurons, eqs, threshold='v>0.8', reset='v = 0', method='exact')
 
-G.tau = '10*ms + i*ms'
-G.add_attribute('logp')
-G.add_attribute('molwt')
-G.molwt = molwt  # Set molecular weight for each neuron
-G.logp = logp
-M = StateMonitor(G, 'v', record=True)
-run(duration)
+
 #####
 ds = SupervisedDataSet(input_neurons, output_neurons)
-ds.addSample(M.v[:, -1], molwt)
 net = buildNetwork(input_neurons, hidden_neurons_1, output_neurons)
 trainer = BackpropTrainer(net, ds)
 
-for a in range(1000):
-    trainer.train()
 
-# Test the network
-for input, target in ds:
-    print(input, target)
-    prediction = net.activate(input)
-    print(f"Input: {input}, Prediction: {prediction}")
+class Integrator:
+    def __init__(self, y0, deriv, t, dt):
+        self.deriv = deriv
+        self.y = y0[:]  # Single neuron's state
+        self.dt = dt
+        self.t = t
+        # Initialize with random values between -1.0 and 1.0
+        self._w = [random.randrange(-1000000000000000, 1000000000000000)/1000000000000000 for _ in range(len(y0))]
+        self._k1 = [random.randrange(-1000000000000000, 1000000000000000)/1000000000000000 for _ in range(len(y0))]
+        self._k2 = [random.randrange(-1000000000000000, 1000000000000000)/1000000000000000 for _ in range(len(y0))]
+        self._k3 = [random.randrange(-1000000000000000, 1000000000000000)/1000000000000000 for _ in range(len(y0))]
+        self._k4 = [random.randrange(-1000000000000000, 1000000000000000)/1000000000000000 for _ in range(len(y0))]
+
+    def step(self):
+        self.deriv(self._k1, self.y, self.t)
+        for i in range(len(self.y)):
+            self._w[i] = self.y[i] + 0.5 * self.dt * self._k1[i]
+        self.deriv(self._k2, self._w, self.t + 0.5 * self.dt)
+        for i in range(len(self.y)):
+            self._w[i] = self.y[i] + 0.5 * self.dt * self._k2[i]
+        self.deriv(self._k3, self._w, self.t + 0.5 * self.dt)
+        for i in range(len(self.y)):
+            self._w[i] = self.y[i] + self.dt * self._k3[i]
+        self.deriv(self._k4, self._w, self.t + self.dt)
+        dto6 = self.dt / 6.0
+        for i in range(len(self.y)):
+            self.y[i] += dto6 * (self._k1[i] + 2*self._k2[i] + 2*self._k3[i] + self._k4[i])
+        self.t += self.dt
+        return self
+
+    def steps(self, n):
+        for _ in range(n):
+            self.step()
+        return self
+
+def IntegratorFactory(y0, deriv, t, dt):
+    return Integrator(y0, deriv, t, dt)
+
+def example_deriv(out, y, t):
+    for i in range(len(y)):
+        out[i] = (molwt + logp - y[i]) / tau # "dv/dt = (molwt + logp - v) / tau" 
+
+# === Model parameters ===
+tau     = 1.0              # time constant
+v_th    = 0.8              # threshold voltage (Brian2-style)
+v_reset = 0.0              # reset voltage
+n_neurons = 2              # Number of neurons (to track spikes for each one)
+y0 = [random.randrange(-1000000000000000, 1000000000000000)/1000000000000000 for _ in range(n_neurons)] 
+t0 = 0.0
+dt = 0.01
+n_steps = 1000  # simulate for 10 ms if dt=0.01
+spikes=[[] for _ in range(n_neurons)]
+neurons=[[] for _ in range(n_neurons)]
+output_text = f"Spike times per neuron:\n"
+for _ in range(n_steps):
+    for neuron_idx in range(n_neurons):
+        integrator = IntegratorFactory([random.randrange(-1000000000000000, 1000000000000000)/1000000000000000], example_deriv, t0, dt)
+        integrator.step()
+        v = integrator.y[0]
+        t = integrator.t
+        if v >= v_th:
+            spikes[neuron_idx].append(v)
+            integrator.y[0] = v_reset  # Reset voltage
+        neurons[neuron_idx].append(integrator)
+
+ds.addSample([neurons[0][-1].y[0], neurons[-1][-1].y[0]], [molwt])
+
+for i in range(n_steps):
+    print(i, trainer.train())
+    
+print(net.activate([neurons[0][-1].y[0], neurons[-1][-1].y[0]]))
